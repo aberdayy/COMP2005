851859d9f2526b6a43b1b4b819dec9e253a14142
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-10-24 16:41:58 +0300

    week 1

---
+++ week_1.py
@@ -0,0 +1,9 @@
+#week 1
+
+# example for reminding
+
+name = input("enter your name = \n")
+print(f"hello {name}\n")
+year = input("Enter your birthyear\n")
+age = 2023-int(year)
+print(f"Hello {name} you are {age} years old.")
